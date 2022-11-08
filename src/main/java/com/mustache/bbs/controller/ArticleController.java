4002d4078f2e427f20ca73cda651218dceaf1ac1
S2ujeong <sj980204@gmail.com>
2022-11-08 15:40:12 +0900

    조회기능만들기

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -4,14 +4,19 @@ import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.domain.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/articles")
+
 @Slf4j
 public class ArticleController {
 
@@ -22,9 +27,32 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
+    @GetMapping(value = "/list")
+    public String getAll(Model model) {
+        List<Article> articleList = (List<Article>) articleRepository.findAll();
+        model.addAttribute("articleList", articleList);
+        return "list";
+    }
+
+    @GetMapping(value = "")  // http://localhost:8080/articles 호출 시 자동으로 http://localhost:8080/articles/list 로 실행
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
+    }
+
+    @GetMapping(value = "/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
     }
 
     @PostMapping(value = "/posts")
@@ -33,7 +61,6 @@ public class ArticleController {
         Article article = form.toEntity();  // 1. Dto를 Entity로 변환
         Article saved = articleRepository.save(article);    // 2. Repository에게 Entity를 DB에 저장하게 한다.
         log.info("generatedId:{}", saved.getId());
-        // return "redirect:/articles/" + saved.getId();  // 리다이렉트 코드, 메서드 작동이 끝나면 /articles/{id} 페이지를 반환한다.
-        return "";
+        return String.format("redirect:/articles/%d", saved.getId());  // 리다이렉트 코드, 메서드 작동이 끝나면 /articles/{id} 페이지를 반환한다.
     }
 }
--- src/main/java/com/mustache/bbs/domain/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/domain/repository/ArticleRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     // < 관리대상엔티티, 대표값의 타입( Aticle의 대표값 id의 타입은 long)>
+    @Override
+    List<Article> findAll(); // 메소드 오버라이딩으로 List 타입으로 바꿔준다.
 }
