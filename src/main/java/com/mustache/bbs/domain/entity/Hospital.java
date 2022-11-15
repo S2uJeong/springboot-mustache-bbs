8374aebb71475fea6e87ec023f3f4383375ddb79
S2ujeong <sj980204@gmail.com>
2022-11-15 17:42:25 +0900

    병원 정보 조회하기 2. page기술 이용해서 list로 보여주기

--- src/main/java/com/mustache/bbs/domain/entity/Hospital.java
+++ src/main/java/com/mustache/bbs/domain/entity/Hospital.java
@@ -1,11 +1,11 @@
 package com.mustache.bbs.domain.entity;
 
+import jdk.jshell.Snippet;
+import lombok.Builder;
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 @Getter
 @Entity
@@ -13,4 +13,27 @@ import javax.persistence.Table;
 public class Hospital {
     @Id  // GeneratedValue 안씀
     private Integer id; // Long --> bigint
+
+    @Column(name = "hospital_name")
+    private String hospitalName;
+
+    @Column(name = "road_name_address")
+    private String roadNameAddress;
+
+    @Builder
+    public Hospital(Integer id, String hospitalName, String roadNameAddress) {
+        this.id = id;
+        this.hospitalName = hospitalName;
+        this.roadNameAddress = roadNameAddress;
+    }
+
+    public Hospital() {
+
+    }
+
+    public static Hospital of(Integer id) {  // 정적메소드 만들기 안에 빌더??
+        return Hospital.builder()
+                .id(id).build();
+    }
+
 }
